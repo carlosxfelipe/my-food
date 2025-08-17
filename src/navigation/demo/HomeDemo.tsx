67a23c4a9600a2363d4064cfa845e3daadaf8c00
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2025-08-17 15:42:28 -0300

    feat: create HomeDemo component with navigation buttons for Profile and Settings

---
+++ src/navigation/demo/HomeDemo.tsx
@@ -0,0 +1,57 @@
+// import { Button } from "@react-navigation/elements";
+import { StyleSheet } from "react-native";
+import { ThemedView } from "../../components/ThemedView";
+import { ThemedText } from "../../components/ThemedText";
+import { ThemedButton } from "../../components/ThemedButton";
+import { type NavigationProp, useNavigation } from "@react-navigation/native";
+
+export function HomeDemo() {
+  const navigation = useNavigation<
+    NavigationProp<ReactNavigation.RootParamList>
+  >();
+
+  return (
+    <ThemedView style={styles.container}>
+      <ThemedText type="title">Tela Inicial</ThemedText>
+
+      {
+        /* <Button
+        screen="Profile"
+        params={{ user: "carlos felipe" }}
+        style={styles.button}
+      >
+        Ir para Perfil
+      </Button> */
+      }
+
+      <ThemedButton
+        title="Ir para Perfil"
+        onPress={() =>
+          navigation.navigate("Profile", { user: "carlos felipe" })}
+        buttonStyle={styles.button}
+      />
+
+      {
+        /* <Button screen="Settings" style={styles.button}>
+        Ir para Configurações
+      </Button> */
+      }
+
+      <ThemedButton
+        title="Ir para Configurações"
+        onPress={() => navigation.navigate("Settings")}
+        buttonStyle={styles.button}
+      />
+    </ThemedView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+  button: {
+    marginTop: 10,
+  },
+});
